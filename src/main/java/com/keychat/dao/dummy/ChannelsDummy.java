b1f2a63b70aef52481e33b71e7e4e9fe7a61dd53
Hyungjo <myexpnfut@gmail.com>
2018-06-04 03:38:13 +0900

    Project Init 2

--- src/main/java/com/keychat/dao/dummy/ChannelsDummy.java
+++ src/main/java/com/keychat/dao/dummy/ChannelsDummy.java
@@ -1,6 +1,28 @@
 package com.keychat.dao.dummy;
 
 import com.keychat.dao.base.ChannelsDao;
+import com.keychat.dto.base.ChannelsModel;
+
+import java.util.List;
 
 public class ChannelsDummy implements ChannelsDao {
+    @Override
+    public boolean createChannel(ChannelsModel createChannelModel) {
+        return false;
+    }
+
+    @Override
+    public List<ChannelsModel> readChannels(ChannelsModel readChannelModel) {
+        return null;
+    }
+
+    @Override
+    public boolean updateChannel(ChannelsModel searchChannelModel, ChannelsModel updateChannelModel) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteChannel(ChannelsModel deleteChannelModel) {
+        return false;
+    }
 }
