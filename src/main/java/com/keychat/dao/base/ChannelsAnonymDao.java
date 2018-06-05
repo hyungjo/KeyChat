ad47282ac316c688b4245cd9eb7a47a93a8063b6
USER-PC\USER <qnfkf1234@naver.com>
2018-06-05 17:46:00 +0900

    dao수정

--- src/main/java/com/keychat/dao/base/ChannelsAnonymDao.java
+++ src/main/java/com/keychat/dao/base/ChannelsAnonymDao.java
@@ -28,32 +28,5 @@ public class ChannelsAnonymDao {
 				DBUtil.close(pstmt, con);
 			}
 		}
-		//이거어제 너가만든건디 햇갈린다 ㅋㅋㅋㅋㅋㅋ 이건 좀있다가해야겟다 급해서
-		public static boolean findLimitAnonym() throws SQLException {
-			Connection conn = null;
-			PreparedStatement pstmt = null;
-			ResultSet rset = null;
-			ArrayList<ChannelsAnonymModel> allList = new ArrayList<ChannelsAnonymModel>();
-			int reuslt;
-			boolean r = false; // 비활성화
-			String sql = "select * from channels";
-			try {
-				conn = DBUtil.getConnection();
-				pstmt = conn.prepareStatement(sql);
-				rset = pstmt.executeQuery();
-				while (rset.next()) {
-					allList.add(new ChannelsAnonymModel(rset.getString(1), rset.getString(2), rset.getInt(3), rset.getInt(4),rset.getInt(5),rset.getInt(6),rset.getDate(7)));
-					reuslt = rset.getInt(6);
-					if(reuslt == 1) { //  익명 활성화
-						r = true;
-					}
-				}
-			} catch (SQLException sqle) {
-				sqle.printStackTrace();
-				throw sqle;
-			} finally {
-				DBUtil.close(rset, pstmt, conn);
-			}
-			return r;
-		}
+		
 	}
