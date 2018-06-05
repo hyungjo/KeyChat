e7317bfca99a5c3af3a95a96cf81781f61320bfd
hyungjo <myexpnfut@gmail.com>
2018-06-05 18:00:00 +0900

    익명 dao 수정

--- src/main/java/com/keychat/dao/base/ChannelsAnonymDao.java
+++ src/main/java/com/keychat/dao/base/ChannelsAnonymDao.java
@@ -1,30 +1,6 @@
 package com.keychat.dao.base;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-
-import com.keychat.controller.util.DBUtil;
-import com.keychat.dto.base.ChannelsAnonymModel;
-
 public class ChannelsAnonymDao {
-	// CHANNELS_ANONYM에서 EMAIL을 찾아 회원을 탈퇴 한다.1
-	public static void dropChannelsAnonym(String email) throws SQLException {
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		String query = "DELETE FROM CHANNELS_ANONYM WHERE EMAIL=?";
-		try {
-			con = DBUtil.getConnection();
-			pstmt = con.prepareStatement(query);
-			pstmt.setString(1, email);
-			pstmt.executeQuery();
-		} catch (SQLException s) {
-			s.printStackTrace();
-			throw s;
-		} finally {
-			DBUtil.close(pstmt, con);
-		}
-	}
+	public static void dropChannelsAnonym(String email)  {
+
 }
