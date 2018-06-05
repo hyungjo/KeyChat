74aaa09e511ddd330d948975872740a1f2e01dc2
hyungjo <myexpnfut@gmail.com>
2018-06-05 14:46:17 +0900

    Response Model 추가

--- src/main/java/com/keychat/dto/util/ResponseModel.java
+++ src/main/java/com/keychat/dto/util/ResponseModel.java
@@ -1,4 +1,13 @@
 package com.keychat.dto.util;
 
 public class ResponseModel {
+    private int status;
+    private String statusMsg;
+    private String result;
+
+    public ResponseModel(int status, String statusMsg, String result){
+        this.status = status;
+        this.statusMsg = statusMsg;
+        this.result = result;
+    }
 }
