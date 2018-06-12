8cec18500cc460dd7295aaf6a6c2f508f6a95e32
hyungjo <myexpnfut@gmail.com>
2018-06-12 14:16:53 +0900

    Signin/Signout 기능 구현

--- src/main/java/com/keychat/controller/user/UserSigninController.java
+++ src/main/java/com/keychat/controller/user/UserSigninController.java
@@ -11,6 +11,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = "/user/signin")
@@ -21,10 +22,13 @@ public class UserSigninController extends HttpServlet {
         UsersModel usersModel = JsonUtil.getModelFromJsonRequest(request, UsersModel.class);
         boolean isExist = UsersDao.isExactPassword(usersModel);
         UsersModel user = UsersDao.getUser(usersModel);
-        if(isExist && user != null)
+        if(isExist && user != null) {
+            HttpSession session = request.getSession();
+            session.setAttribute("loginUser", user);
             res = new ResponseModel(200, "success", user);
+        }
         else
-            res = new ResponseModel(500, "fail", "Cannot create user");
+            res = new ResponseModel(500, "fail", "Cannot signin user");
 
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
--- src/main/java/com/keychat/controller/user/UserSignoutController.java
+++ src/main/java/com/keychat/controller/user/UserSignoutController.java
@@ -1,29 +1,36 @@
 package com.keychat.controller.user;
 
+import com.google.gson.Gson;
+import com.keychat.controller.util.JsonUtil;
+import com.keychat.dao.base.UsersDao;
+import com.keychat.dto.base.UsersModel;
+import com.keychat.dto.util.ResponseModel;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = "/user/signout")
 public class UserSignoutController extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        ResponseModel res;
 
-    }
+        HttpSession session = request.getSession();
+        UsersModel loginUser = (UsersModel)session.getAttribute("loginUser");
+        if(loginUser != null) {
+            session.invalidate();
+            res = new ResponseModel(200, "success", loginUser);
+        }
+        else
+            res = new ResponseModel(500, "fail", "Cannot signout user");
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
-
-    @Override
-    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPut(req, resp);
-    }
-
-    @Override
-    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doDelete(req, resp);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(new Gson().toJson(res));
     }
 }
